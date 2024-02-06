c62a8f16539fdb85a50266a63a63db008040af61
NesterovSerhii <121351473+NesterovSerhii@users.noreply.github.com>
2024-02-06 12:37:31 +0200

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import Header from './Header/';
+import css from './App.styled.module.css'
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <Header/>
     </div>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import css from './Header.styled.module.css'
+const Header = () => {
+  return (
+    <header className={css['header-container']}>
+        <div>
+            <a href="/" className={css.logo}>Nanny.Services</a>
+        </div>
+        <nav className={css.nav}>
+            <div className={css['header-nav-links']}>
+                <a href="/">Home</a>
+                <a href="/nannies">Nannies</a>
+            </div>
+            <ul className={css['header-buttons']}>
+               <li>
+                <button type='button' className={css['header-login-button']}>Log in</button>
+                </li>
+               <li>
+                <button type='button' className={css['header-reg-button']}>Registration</button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
