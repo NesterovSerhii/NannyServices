a2b214c79ad012ca64de685d15ebb0f198595f6e
NesterovSerhii <sergei_nesterov8@ukr.net>
2024-02-13 10:03:53 +0200

    add forms validation

--- src/components/SignInModal/SignInModal.jsx
+++ src/components/SignInModal/SignInModal.jsx
@@ -1,15 +1,37 @@
-import React, { useEffect } from 'react';
- import { useFormik } from 'formik';
+import React, { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+ import * as Yup from 'yup';
 import css from './SignInModal.module.css';
 import closeBtnIcon from '../../assets/icons/closeBtn.svg'
+import eyeClosedIcon from '../../assets/icons/eye-off.svg';
+import eyeIcon from '../../assets/icons/eye.svg';
+
+const passwordRegExp =/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/;
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+
+export const basicSchema = Yup.object().shape({
+  email: Yup
+    .string()
+    .matches(emailRegexp, 'Enter a valid Email')
+    .email('Enter a valid Email')
+    .required('Email is required'),
+  password: Yup
+    .string()
+    .required('Password is required')
+    .matches(passwordRegExp, 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. It should be 8 to 32 characters long.'),
+});
 
 const SignInModal = ({ onClose }) => {
+   const [showPassword, setShowPassword] = useState(false);
 
 const formik = useFormik({
      initialValues: {
        email: '',
        password: '',
-     },
+  },
+  validationSchema: basicSchema,
      onSubmit: values => {
        alert(JSON.stringify(values, null, 2));
      },
@@ -48,24 +70,47 @@ const formik = useFormik({
         <p className={css.formText}>Welcome back! Please enter your credentials to access your account and continue your babysitter search.</p>
 
          <form onSubmit={formik.handleSubmit}>
-       <input className={css.formInput}
-         id="email"
-         name="email"
-         type="email"
-         placeholder='Email'
-         onChange={formik.handleChange}
-         value={formik.values.email}
-       />
-       <input className={css.formInput}
-         id="password"
-         name="password"
-         type="password"
-         placeholder='Password'
-         onChange={formik.handleChange}
-         value={formik.values.password}
-       />
-       <button className={css.formBtn} type="submit">Log In</button>
-     </form>
+          <input
+            className={`${css.formInput} ${formik.errors.email && formik.touched.email ? css.formInputError : ''}`}
+            id="email"
+            name="email"
+            type="email"
+            placeholder='Email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            autoComplete='off'
+          />
+          <div className={css.errorMsg}>
+            {formik.errors.email && formik.touched.email && (
+              <div className={css.inputError}>{formik.errors.email}</div>
+            )}
+          </div>
+          <div className={css.passInputWrap}>
+          <input
+            className={`${css.formInput} ${css.formInputPassword} ${formik.errors.password && formik.touched.password ? css.formInputError : ''}`}
+            id="password"
+            name="password"
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            autoComplete='off'
+          />
+          <button type="button" onClick={() => setShowPassword(!showPassword)} className={css.togglePasswordBtn}>
+            {showPassword ? (
+              <img src={eyeClosedIcon} alt="Hide" />
+            ) : (
+              <img src={eyeIcon} alt="Show" />
+            )}
+          </button>
+          </div>
+          <div className={css.errorMsg}>
+            {formik.errors.password && formik.touched.password && (
+              <div className={css.inputError}>{formik.errors.password}</div>
+            )}
+          </div>
+          <button className={css.formBtn} type="submit">Sign Up</button>
+        </form>
       </div>
     </div>
   );
--- src/components/SignUpModal/SignUpModal.jsx
+++ src/components/SignUpModal/SignUpModal.jsx
@@ -1,9 +1,31 @@
-import React, { useEffect } from 'react';
- import { useFormik } from 'formik';
+import React, { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import css from './SignUpModal.module.css';
 import closeBtnIcon from '../../assets/icons/closeBtn.svg'
+import eyeClosedIcon from '../../assets/icons/eye-off.svg';
+import eyeIcon from '../../assets/icons/eye.svg';
+
+const passwordRegExp =/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/;
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+
+export const basicSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required').min(2).max(32),
+  email: Yup
+    .string()
+    .matches(emailRegexp, 'Enter a valid Email')
+    .email('Enter a valid Email')
+    .required('Email is required'),
+  password: Yup
+    .string()
+    .required('Password is required')
+    .matches(passwordRegExp, 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. It should be 8 to 32 characters long.'),
+});
 
 const SignUpModal = ({ onClose }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
    const formik = useFormik({
      initialValues: {
@@ -11,6 +33,7 @@ const SignUpModal = ({ onClose }) => {
        email: '',
        password: '',
      },
+      validationSchema: basicSchema,
      onSubmit: values => {
        alert(JSON.stringify(values, null, 2));
      },
@@ -43,38 +66,68 @@ const SignUpModal = ({ onClose }) => {
       <div className={css.modal}>
 
         <button className={css.closeButton} onClick={onClose}>
-  <img src={closeBtnIcon} alt="Close" />
-</button>
+          <img src={closeBtnIcon} alt="Close" />
+        </button>
         <h2 className={css.formTitle}>Registration</h2>
         <p className={css.formText}>Thank you for your interest in our platform! In order to register, we need some information. Please provide us with the following information.</p>
 
-         <form onSubmit={formik.handleSubmit}>
-       <input className={css.formInput}
-         id="name"
-         name="name"
-         type="text"
-         placeholder='Name'
-         onChange={formik.handleChange}
-         value={formik.values.name}
-       />
-       <input className={css.formInput}
-         id="email"
-         name="email"
-         type="email"
-         placeholder='Email'
-         onChange={formik.handleChange}
-         value={formik.values.email}
-       />
-       <input className={css.formInput}
-         id="password"
-         name="password"
-         type="password"
-         placeholder='Password'
-         onChange={formik.handleChange}
-         value={formik.values.password}
-       />
-       <button className={css.formBtn} type="submit">Sign Up</button>
-     </form>
+        <form onSubmit={formik.handleSubmit}>
+          <input
+            className={`${css.formInput} ${formik.errors.name && formik.touched.name ? css.formInputError : ''}`}
+            id="name"
+            name="name"
+            type="text"
+            placeholder='Name'
+            autoComplete='off'
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          <div className={css.errorMsg}>
+            {formik.errors.name && formik.touched.name && (
+              <div className={css.inputError}>{formik.errors.name}</div>
+            )}
+          </div>
+          <input
+            className={`${css.formInput} ${formik.errors.email && formik.touched.email ? css.formInputError : ''}`}
+            id="email"
+            name="email"
+            type="email"
+            placeholder='Email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            autoComplete='off'
+          />
+          <div className={css.errorMsg}>
+            {formik.errors.email && formik.touched.email && (
+              <div className={css.inputError}>{formik.errors.email}</div>
+            )}
+          </div>
+          <div className={css.passInputWrap}>
+          <input
+            className={`${css.formInput} ${css.formInputPassword} ${formik.errors.password && formik.touched.password ? css.formInputError : ''}`}
+            id="password"
+            name="password"
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            autoComplete='off'
+          />
+          <button type="button" onClick={() => setShowPassword(!showPassword)} className={css.togglePasswordBtn}>
+            {showPassword ? (
+              <img src={eyeClosedIcon} alt="Hide" />
+            ) : (
+              <img src={eyeIcon} alt="Show" />
+            )}
+          </button>
+          </div>
+          <div className={css.errorMsg}>
+            {formik.errors.password && formik.touched.password && (
+              <div className={css.inputError}>{formik.errors.password}</div>
+            )}
+          </div>
+          <button className={css.formBtn} type="submit">Sign Up</button>
+        </form>
       </div>
     </div>
   );
